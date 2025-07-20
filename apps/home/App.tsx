eaeb92a96a091dd28cf84d3a0b017c97ec1a0625
SEONGMINY <leesm2075@naver.com>
2025-07-20 17:28:32 +0900

    refactor: Home 코드 노이즈 제거

--- apps/home/App.tsx
+++ apps/home/App.tsx
@@ -1,38 +1,12 @@
 import React from 'react';
-import {ScrollView, StatusBar, Text, useColorScheme, View,} from 'react-native';
-
-import {Colors, Header, LearnMoreLinks,} from 'react-native/Libraries/NewAppScreen';
+import {Text, View,} from 'react-native';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-  const safePadding = '5%';
 
   return (
-    <View style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        style={backgroundStyle}>
-        <View style={{paddingRight: safePadding}}>
-          <Header/>
-        </View>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-            paddingHorizontal: safePadding,
-            paddingBottom: safePadding,
-          }}>
-            <Text>여기는 Home 앱 입니다</Text>
-            <Text>MINI 앱 테스트</Text>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <View>
+        <Text>여기는 Home 앱 입니다</Text>
+        <Text>MINI 앱 테스트</Text>
     </View>
   );
 }
